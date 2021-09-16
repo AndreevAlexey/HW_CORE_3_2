fe65ec28abf34f587c4a5059e43dba1bad03a758
Andreev Alexey <andreev-j@yandex.ru>
2021-09-16 21:12:49 +0700

    Update1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,64 +1,65 @@
 package com.company;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
     final static String ROOT_DIR = "C://games//savegames//";
     final static String FILE_END = ".dat";
+    static StringBuilder LOG;
     // сохранить состояние объекта в файл
     static void saveProgress(GameProgress save, String name) {
         // потоки
-        try(FileOutputStream fos = new FileOutputStream(ROOT_DIR + name);
+        try(FileOutputStream fos = new FileOutputStream(name);
             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             // сохранение
             oos.writeObject(save);
         } catch (IOException exp) {
-            System.out.println("Ошибка сохранения файла " + ROOT_DIR + name + " :" + exp.getMessage());
+            LOG.append("\nОшибка сохранения файла ").append(name).append(" :").append(exp.getMessage());
         }
     }
     // Архивация
-    static void zipFiles(File dir) {
-        // передана директория
-        if(dir.isDirectory()) {
-            try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(ROOT_DIR + "saves.zip"))) {
-                // цикл по файлам рабочей директории
-                for (File file : dir.listFiles((a, b) -> b.endsWith(FILE_END))) {
-                    // поток данных файла
-                    FileInputStream fis = new FileInputStream(file);
-                    // вхождение файла в архив
-                    ZipEntry entry = new ZipEntry(file.getName());
-                    zos.putNextEntry(entry);
-                    // чтение-запись данных файла
-                    byte[] buffer = new byte[fis.available()];
-                    if (fis.read(buffer) != -1) zos.write(buffer);
-                    // закрытие вхождения
-                    zos.closeEntry();
-                    // закрытие потока
-                    fis.close();
-                }
-                // удаление файлов вне архива, после архивации
-                for (File file : dir.listFiles()) {
-                    // имя файла
-                    String name = file.getName();
-                    // если не архив
-                    if (!name.endsWith(".zip")) {
-                        // удаляем
-                        if(!file.delete()) System.out.println("Не удалось удалить файл: " + file.getName());
-                    }
-                }
-            } catch (IOException exp) {
-                System.out.println("Ошибка архивирования файлов: " + exp.getMessage());
+    static void zipFiles(String path, ArrayList<String> files) {
+        // архивация
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(path + "//saves.zip"))) {
+            // цикл по файлам
+            for(String file : files) {
+                // текущий файл
+                File curFile = new File(file);
+                // поток данных файла
+                FileInputStream fis = new FileInputStream(curFile);
+                // вхождение файла в архив
+                ZipEntry entry = new ZipEntry(curFile.getName());
+                zos.putNextEntry(entry);
+                // чтение-запись данных файла
+                byte[] buffer = new byte[fis.available()];
+                if (fis.read(buffer) != -1) zos.write(buffer);
+                // закрытие вхождения
+                zos.closeEntry();
+                // закрытие потока
+                fis.close();
             }
+            // удаление файлов вне архива, после архивации
+            for(String rec : files) {
+                File file = new File(rec);
+                // удаляем
+                if(!file.delete()) LOG.append("\nНе удалось удалить файл: ").append(file.getName());
+            }
+        } catch (IOException exp) {
+            LOG.append("\nОшибка архивирования файлов: ").append(exp.getMessage());
         }
     }
 
     public static void main(String[] args) {
         boolean dirExist;
+        String fileName;
+        ArrayList<String> filesToZip = new ArrayList<>();
         GameProgress[] saves = {new GameProgress(100, 100, 1, 1),
                                 new GameProgress(90, 90, 2, 2),
                                 new GameProgress(80, 80, 3, 3)};
+        LOG = new StringBuilder();
         // рабдочая директория
         File wrkDir = new File(ROOT_DIR);
         dirExist = wrkDir.exists();
@@ -68,12 +69,14 @@ public class Main {
         if(dirExist) {
             // цикл по объектам
             for(int i = 0; i < saves.length; i++) {
+                fileName = wrkDir + "//save" + i + FILE_END;
                 // сохранение в файл состояний объектов
-                saveProgress(saves[i], "save"+i+".dat");
+                saveProgress(saves[i], fileName);
+                filesToZip.add(fileName);
             }
             // Архивация
-            zipFiles(wrkDir);
-        } else
-            System.out.println("Директории " + ROOT_DIR + " не существует, сохранение невозможно!");
+            zipFiles(wrkDir.getPath(), filesToZip);
+        } else LOG.append("\nДиректории ").append(ROOT_DIR).append(" не существует, сохранение невозможно!");
+        if(!LOG.isEmpty()) System.out.println(LOG.toString());
     }
 }
